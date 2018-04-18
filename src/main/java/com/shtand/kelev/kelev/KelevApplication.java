6b0f08f4429702425920fbcdc334c78c71c6728b
Uri Shtand <uri.shtand@gmail.com>
2018-04-18 13:32:38 +0300

    SpringBoot starter

---
+++ src/main/java/com/shtand/kelev/kelev/KelevApplication.java
@@ -0,0 +1,12 @@
+package com.shtand.kelev.kelev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KelevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KelevApplication.class, args);
+	}
+}
---
+++ src/test/java/com/shtand/kelev/kelev/KelevApplicationTests.java
@@ -0,0 +1,16 @@
+package com.shtand.kelev.kelev;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KelevApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
